037bb3e443215a935a4f20d9dee0fb79cbfa9c8e
Matthias Kricke <merando@gmx.de>
2017-05-08 14:14:57 +0200

    removed not used class

--- src/main/java/org/gradoop/examples/io/dblp/ExtractionHelper.java
+++
@@ -1,8 +0,0 @@
-package org.gradoop.examples.io.dblp;
-
-/**
- * Some Util Methods for easier extraction of fields from {@link org.dblp.datastructures.DblpElement}
- */
-public class ExtractionHelper {
-
-}
